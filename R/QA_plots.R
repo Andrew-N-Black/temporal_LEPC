cbfc7c42ea9629fc37cc1384621d4eebc6cc849e
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-09-21 10:23:27 -0700

    Update QA_plots.R

--- R/QA_plots.R
+++ R/QA_plots.R
@@ -1,9 +1,15 @@
+library(ggplot2)
+library(readxl)
+library(ggpubr)
+library(dplyr)
 
+#By DPS and Species
+#load metadata
+QA <- read_xlsx("/Users/andrewblack/Documents/Research/GROUSE/sarek_old_new/old_new_heterozygosity.xlsx")
 
-depth_breadth <- read_excel("metadata.xlsx")
 #breadth
-ggplot(data=depth_breadth, aes(y=BREADTH_POST, x=reorder(ID,BREADTH_POST),fill=SPECIES))+geom_bar(stat="identity")+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown","Tympanuchus cupido"="goldenrod"))+xlab("Sample (N=481)")+ylab("Genome Breadth")+theme( axis.ticks.x=element_blank(),axis.text.x = element_blank())+ geom_hline(yintercept=80, linetype="dashed", color = "white", linewidth=1)+ylim(0,100)+theme(legend.position="none")
-#mapping
-ggplot(data=depth_breadth, aes(y=MAPPING_TOTAL, x=reorder(ID,MAPPING_TOTAL),fill=SPECIES))+geom_bar(stat="identity")+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown","Tympanuchus cupido"="goldenrod"))+xlab("Sample (N=481)")+ylab("Reads Aligned (%)")+theme( axis.ticks.x=element_blank(),axis.text.x = element_blank())+ylim(c(0,100))+ geom_hline(yintercept=80, linetype="dashed", color = "white", linewidth=1)+theme(legend.position="top")
-#depth
-ggplot(data=depth_breadth, aes(y=DEPTH_POST, x=reorder(ID,DEPTH_POST),fill=SPECIES))+geom_bar(stat="identity")+scale_fill_manual("", values =c("Tympanuchus pallidicinctus"="brown","Tympanuchus cupido"="goldenrod"))+xlab("Sample (N=481)")+ylab("Depth Of Coverage")+theme( axis.ticks.x=element_blank(),axis.text.x = element_blank())+ geom_hline(yintercept=3, linetype="dashed", color = "white", linewidth=1)+theme(legend.position="top")
+ggplot(data=QA, aes(y=DOC, x=reorder(ID,DOC),fill=GRP, label = "mean=18.85, range=9.9-25.4"))+geom_bar(stat="identity")+scale_fill_manual("", values =c("Past"="cadetblue","Present"="black"))+xlab("Sample (N=20)")+ylab("Depth of Coverage")+theme(legend.position="top")+theme_classic()+annotate("text", x = 10, y = 30, label = "mean=18.85, range=9.88-25.4",color = "grey", size = 4, fontface = "italic")+ylim(0,30)
+
+#Mapping
+ggplot(data=QA, aes(y=properlyPaired, x=reorder(ID,properlyPaired),fill=GRP, label = "mean=18.85, range=9.9-25.4"))+geom_bar(stat="identity")+scale_fill_manual("", values =c("Past"="cadetblue","Present"="black"))+xlab("Sample (N=20)")+ylab("% Mapped")+theme(legend.position="top")+theme_classic()+annotate("text", x = 10, y = 100, label = "mean=96%, range=84.4%-98.9%",color = "grey", size = 4, fontface = "italic")+ylim(0,100)
+
